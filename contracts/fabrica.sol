a5c51dc77de225ca4e640f348a4aefac73dae4a8
gemt33 <78438718+gemt33@users.noreply.github.com>
2021-03-16 14:43:53 +0100

    pragma & import openZep ERC20

---
+++ contracts/fabrica.sol
@@ -0,0 +1,4 @@
+pragma solidity ^0.6.6; // patrick advises to use this version. This is a stable and well documented one.
+
+// OpenZepplin ERC20
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
