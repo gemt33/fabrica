cdb2f7a56d6eefb27c095c51e77d4b91001fd604
gemt33 <78438718+gemt33@users.noreply.github.com>
2021-03-21 18:56:58 +0100

    Create fabricaPool.sol

---
+++ contracts/fabricaPool.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.6.6; // patrick advises to use this version. This is a stable and well documented one.
+
+// import Fabrica token
+import "./fabrica.sol";
+
+// this contract is used to create the pool of fab. People will need to send their tokens to this contract to unlock posts
+contract FabricaPool {
+    constructor() public {};
+}
